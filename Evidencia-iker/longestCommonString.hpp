d2f14a887cddf04edaf73ef4ef113351f2f1326a
iker99 <iker200409@gmail.com>
2024-09-26 23:13:08 -0600

    IkerParte3Final2

--- Evidencia-iker/longestCommonString.hpp
+++ Evidencia-iker/longestCommonString.hpp
@@ -10,8 +10,8 @@ using namespace std;
  * Complejidad de espacio: O(n * m), debido a la matriz DP utilizada para almacenar las longitudes de las cadenas comunes.
  */
 string longestCommonString(const string& text1, const string& text2) {
-    int n = text1.size();  // Tamaño del primer texto
-    int m = text2.size();  // Tamaño del segundo texto
+    int n = text1.size();  
+    int m = text2.size();  
     
     // Matriz DP para almacenar las longitudes de cadenas comunes
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));  
@@ -29,33 +29,24 @@ string longestCommonString(const string& text1, const string& text2) {
     // Complejidad de tiempo de la matriz DP: O(n * m)
 
     string result = "";
-    vector<int> posI, posJ;  // Vectores para almacenar las posiciones iniciales
-    int maxLen = 0;  // Longitud máxima de la cadena común
+    int posI;  
+    int maxLen = 0; 
 
     // Encontrar la cadena común más larga
     for (int i = 1; i <= n; i++) {  
         for (int j = 1; j <= m; j++) {  
             if (dp[i][j] > maxLen) {
                 maxLen = dp[i][j];
-                result = text1.substr(i - maxLen, maxLen);  // Actualización del resultado
-
-                // Limpiar y almacenar nuevas posiciones
-                posI.clear();
-                posJ.clear();
-                posI.push_back(i - maxLen);
-                posJ.push_back(j - maxLen);
+                result = text1.substr(i - maxLen, maxLen);  
+                
+                posI = i - maxLen;
             }
         }
     }
     // Complejidad de tiempo para encontrar la cadena más larga: O(n * m)
 
-    // Imprimir posiciones de inicio y final para ambas cadenas
-    for (int i = 0; i < posI.size(); i++) {  
-        cout << "transmission1: " << "Initial position: " << posI[i] + 1 
-            << " Final position: " << posI[i] + maxLen << endl;
-        cout << "transmission2: " << "Initial position: " << posJ[i] + 1 
-            << " Final position: " << posJ[i] + maxLen << endl;
-    }
+    cout << "transmission1: " << "Initial position: " << posI + 1 
+        << " Final position: " << posI + maxLen << endl;
 
     return result;  
 }
