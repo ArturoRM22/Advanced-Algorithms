22a6463c40728495f300ecf7204345177b73011b
ArturoRM22 <A01643269@tec.mx>
2024-11-24 14:42:25 -0600

    TSP solved (dp approach)

---
+++ Integradora2/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include"TSP.hpp"
+using namespace std; 
+
+//TSP struct
+
+int main() {
+    int n; // number of cities
+    cin >> n;
+    vector<vector<int>> distances(n, vector<int>(n));
+    
+    // Read distance matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> distances[i][j];
+        }
+    }
+    
+
+    // Optionally get the path
+    COST_AND_PATH result = TSP(distances);
+    cout << "Cost: " << result.cost << endl;
+    cout << "Path: ";
+    for(int city : result.path) {
+        cout << city << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
